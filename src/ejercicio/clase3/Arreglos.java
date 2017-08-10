d59443bfbf918b94b1f1bcc1ab9a872e24140cac
ESTUDIANTE <ESTUDIANTE@DESKTOP-KTO51U0>
2017-08-09 20:56:48 -0500

    Ejercicios clase 3

---
+++ src/ejercicio/clase3/Arreglos.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.clase3;
+import java.util.*;
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Arreglos {
+    
+
+ public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner flujoEnt = new Scanner (System.in);
+        // declarar varibles
+        
+       double Patrimonio = 0;
+       double RentaLiquida = 0;
+       double ValorCredito = 0;
+       
+       
+        
+        int [] info  = new int [6];
+        
+        System.out.println("Patrimonio:");
+        Patrimonio = flujoEnt.nextDouble();
+        
+        int info [1] =  Patrimonio ;
+        
+        
+        System.out.println("Renta Liquida: ");
+        RentaLiquida = flujoEnt.nextDouble();
+        
+        
+        
+ } 
+   public static double credi (double Patrimonio, double RentaLiquida ){
+       
+       
+       
+   }
+}
