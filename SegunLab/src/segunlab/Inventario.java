838c34d8465aaf420775e024ec3bfe7d702eda3d
lpvelasquezmo <30605014+lpvelasquezmo@users.noreply.github.com>
2017-09-10 02:47:59 -0500

    Update Inventario.java

--- SegunLab/src/segunlab/Inventario.java
+++ SegunLab/src/segunlab/Inventario.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,13 +11,15 @@ package segunlab;
  * @author MARIAJOSE
  */
 public class Inventario {
-    Producto[] producto;
+    Producto[] productos;
+    Proveedor [] proveedores;
+    FechaHora fecha1;
+   
     private int cantidad1;
+    
 
-    public Inventario( Producto producto, int cantidad1) {
-        this.producto = new Producto[cantidad1];
-        this.cantidad1= cantidad1++;
-    }
+    
+    
 
     public int getCantidad1() {
         return cantidad1;
@@ -28,8 +31,8 @@ public class Inventario {
     
     public void agotado (){
         for (int i = 0; i < cantidad1; i++) {
-            if (this.producto[cantidad1].getCantidad() <= 0){
-                System.out.println("El producto" + this.producto[cantidad1].getNombreProd() + "esta agotado");
+            if (this.productos[i].getCantidad() <= 0){
+                System.out.println("El producto" + this.productos[i].getNombreProd() + "esta agotado");
             } else {
                 
             }
@@ -40,14 +43,61 @@ public class Inventario {
     public void masVendido() {
         int cantidadPro = 0, indice = 0;
         for (int y = 0; y < cantidad1; y++) {
-            if (this.producto[y].getCantidad() > cantidadPro) {
-                cantidadPro = this.producto[y].getCantidad();
+            if (this.productos[y].getCantidad() > cantidadPro) {
+                cantidadPro = this.productos[y].getCantidad();
                 indice = y;
             }
         }
+    }
+        public void proxVencerp ( ){
+            for (int y = 0; y < cantidad1; y++) {
+            String fechaN = this.productos[y].getFecha();
+         
+                 float fechaAct, fechaNN;
+         fechaNN  =Float.parseFloat(fechaN);
+         
+         String FechaAct = fecha1.Fecha();
+         fechaAct = Float.parseFloat(FechaAct);
+         for (int h =0; h< cantidad1; h++){
+         if ((fechaAct-fechaNN )< 1){
+             System.out.println("El producto vencera en 1 dia" + productos [y].getNombreProd()); 
+         }
+         }
+     }
             
             
+        }
             
+        public void facturaVencer (){
+        
+        for (int i = 0; i < 20 ; i++) {
+            float fecha2;
+                  String aux1  = this.proveedores[i].getPlazo();
+             fecha2 = Float.parseFloat(aux1);;
+             float fu;
+             String fd = fecha1.Fecha();
+             fu = Float.parseFloat(fd);
+             for (int h =0; h< 20; h++){
+         if ((fu-fecha2 )< 1){
+             System.out.println("la fecha limite vencera en un dia" + proveedores[h].getNombreProveedor()); 
+         }
+          }  
+        }
+    }
+        
+        public void AgregarP ( int numP, String nombre, int cantidad, String fecha, double precio){
+       
+            productos [numP] = new Producto ( nombre, cantidad, fecha, precio);
+            System.out.println("agregado con exito");
+        }
+        
+       public void AgregarPv (int numPv, String nombreProveedor, double deuda, String plazo, boolean cancelado){
+           
+           proveedores [numPv] = new Proveedor (nombreProveedor, deuda, plazo,cancelado);
+       }
+       
+       
+        
 //    public void ActualizarInv(Inventario inventario, int totalVenta, int cant2) {
 //        
 //       
@@ -60,4 +110,4 @@ public class Inventario {
 //        }
 //              }
     
-}}
+}
