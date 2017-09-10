5829b05e6aca8adae988ec206dd2b4d752e7175c
lpvelasquezmo <30605014+lpvelasquezmo@users.noreply.github.com>
2017-09-10 02:51:07 -0500

    Update Producto.java

--- SegunLab/src/segunlab/Producto.java
+++ SegunLab/src/segunlab/Producto.java
@@ -12,11 +12,53 @@ package segunlab;
 public class Producto {
     private String nombreProd;
     private int cantidad;
+    private String fecha;
+    private int vendido;
+    private double precio;
+    private double Dinero;
+    
 
-    public Producto(String nombreProd, int cantidad) {
+    public Producto(String nombreProd, int cantidad, String fecha, double precio) {
         this.nombreProd = nombreProd;
         this.cantidad = cantidad;
+     
+        this.fecha = fecha;
+        this.precio = precio;
     }
+
+    
+    
+    
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getVendido() {
+        return vendido;
+    }
+
+    public void setVendido(int vendido) {
+        this.vendido = vendido;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+   
+
+    
+    
+    
+    
 
     public String getNombreProd() {
         return nombreProd;
@@ -30,5 +72,33 @@ public class Producto {
         this.cantidad = cantidad;
     }
     
+//    static double precioFijo(double precio, String tipo) {
+//        double aux = 0, precioF = 0.0;
+//        switch (tipo) {
+//            case "licor":
+//                aux = (precio * 16) / 100;
+//                precioF = precio + aux;
+//                break;
+//            case "supermercado":
+//                aux = (precio * 4) / 100;
+//                precioF = precio + aux;
+//                break;
+//            
+//            default:
+//                aux = -1.0;
+//                break;
+//        }
+//        return precioF;
+//    }
     
+     public double vender(int cantidad) {
+        
+        this.vendido += cantidad;
+        this.Dinero += precio * cantidad;
+        return precio * cantidad;
+        
+    }
+    
+     
+     
 }
